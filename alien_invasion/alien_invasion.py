154e7f35e199aff411755f5ca7f35b1df401f60a
gnlsw <today_wsl@163.com>
2017-06-11 10:58:03 +0800

    增加子弹相关操作

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 
 def run_game():
@@ -15,13 +16,17 @@ def run_game():
 
     ship = Ship(ai_settings, screen)
 
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
+
     # 设置背景色
     bg_color = (230, 230, 230)
 
     # 开始游戏的主循环
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
