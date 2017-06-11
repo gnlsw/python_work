a3ef6f5f8119afced4e9644d770fc4ec2139d620
gnlsw <today_wsl@163.com>
2017-06-11 21:27:46 +0800

    增加外星人操作

---
+++ alien_invasion/game_stats.py
@@ -0,0 +1,11 @@
+class GameStats():
+    """跟踪游戏的统计信息"""
+
+    def __init__(self, ai_settings):
+        self.ai_settings = ai_settings
+        self.game_active = True
+        self.reset_stats()
+
+    def reset_stats(self):
+        """初始化在油漆期间可能变化的统计信息"""
+        self.ships_left = self.ai_settings.ship_limit
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -3,10 +3,12 @@ class Settings:
 
     def __init__(self):
         self.screen_width = 800
-        self.screen_hegiht = 600
+        self.screen_height = 600
         self.bg_color = (230, 230, 230)
+
         # 飞船的设置
         self.ship_speed_factor = 1.5
+        self.ship_limit = 3
 
         # 子弹设置
         self.bullet_speed_factor = 1
@@ -14,3 +16,10 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullet_allowed = 3
+
+        # 外星人设置
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction为1表示向右移动，-2表示向左移动
+        self.fleet_direction = 1
+
